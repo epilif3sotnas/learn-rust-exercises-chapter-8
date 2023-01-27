01fb54a1c7becb5bf812b2b4c4d9d2d1cea84322
Filipe Santos <filipesantos1999@hotmail.com>
2023-01-27 07:39:14 +0000

    Add function to exercise 2

---
+++ src/exercises/exercise2.rs
@@ -0,0 +1,3 @@
+pub fn exercise_2 (text: &str) {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod exercises;
 
 use crate::exercises::exercise1::exercise_1;
+use crate::exercises::exercise2::exercise_2;
 
 
 fn main() {
@@ -9,5 +10,5 @@ fn main() {
     exercise_1(&vec![1, 2, 3, 412, 123, 123, 123, 23, -1, -123, 1230, 45, 75, 356, 7]);
 
     // exercise 2
-    exercise_2(list);
+    exercise_2("Change to a bigger text.");
 }
