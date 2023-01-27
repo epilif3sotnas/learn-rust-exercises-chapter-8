ef896ee5abba142ebc71634fd15610256bd47138
Filipe Santos <filipesantos1999@hotmail.com>
2023-01-27 21:28:04 +0000

    Add exercise 2, but not working

--- src/exercises/exercise2.rs
+++ src/exercises/exercise2.rs
@@ -1,3 +1,31 @@
 pub fn exercise_2 (text: &str) {
+    let words: Vec<&str> = text.split(" ").collect();
 
+    let mut text_converted: String = String::new();
+
+    for word in words {
+        let word_to_concatenate: String = convert_word(word);
+    }
+}
+
+fn convert_word (word: &str) -> String {
+    let initial_letters: Vec<char> = &word[0..2].chars();
+
+    // ### TODO - Check what rule to apply ###
+}
+
+fn convert_with_rule_1 (word: &str) -> &str {
+
+}
+
+fn convert_with_rule_2 (word: &str) -> &str {
+    
+}
+
+fn convert_with_rule_3 (word: &str) -> &str {
+    
+}
+
+fn convert_with_rule_4 (word: &str) -> &str {
+    
 }
