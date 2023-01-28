d01a3847b901e83f6e4b61048f106f59b4ff7e87
Filipe Santos <filipesantos1999@hotmail.com>
2023-01-28 14:17:51 +0000

    Add exercise 2 rule 1

--- src/exercises/exercise2.rs
+++ src/exercises/exercise2.rs
@@ -9,23 +9,29 @@ pub fn exercise_2 (text: &str) {
 }
 
 fn convert_word (word: &str) -> String {
-    let initial_letters: Vec<char> = &word[0..2].chars();
+    let letters: Vec<char> = word.chars().collect();
 
     // ### TODO - Check what rule to apply ###
 }
 
-fn convert_with_rule_1 (word: &str) -> &str {
+fn convert_with_rule_1 (characters: Vec<char>) -> String {
+    let mut word_compiled: Vec<char> = characters[1..].to_vec();
 
+    word_compiled.push(characters[0]);
+    word_compiled.push('a' as char);
+    word_compiled.push('y' as char);
+
+    return word_compiled.iter().collect();
 }
 
-fn convert_with_rule_2 (word: &str) -> &str {
+fn convert_with_rule_2 (characters: Vec<char>) -> String {
     
 }
 
-fn convert_with_rule_3 (word: &str) -> &str {
+fn convert_with_rule_3 (characters: Vec<char>) -> String {
     
 }
 
-fn convert_with_rule_4 (word: &str) -> &str {
+fn convert_with_rule_4 (characters: Vec<char>) -> String {
     
 }
